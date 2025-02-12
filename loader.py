d1301536cabddfd02fa90aafe51b8943be3d89bc
svillors <tntzator9@gmail.com>
2025-02-12 19:56:15 +0500

    Added styles.css for watching

--- loader.py
+++ loader.py
@@ -9,7 +9,10 @@ def reload():
 server = Server()
 index_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                           'index.html')
+styles_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                           'styles.css')
 server.watch(index_path, reload)
+server.watch(styles_path, reload)
 root_dir = os.path.abspath(os.path.dirname(__file__))
 server.serve(root=root_dir,
              default_filename='index.html',
